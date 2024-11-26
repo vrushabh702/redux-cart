7891aae02442cef550c657997164c35f651fab88
vrushabh <vrushabhp702@gmail.com>
2024-11-26 18:56:39 +0530

    action code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HomeContainer from "./containers/HomeContainer";
 import User from "./user";
 
 function App() {
   return (
     <div className="App">
-      <User data={{ name: "anil sidhu", age: 24 }} />
+      {/* <User data={{ name: "anil sidhu", age: 24 }} /> */}
+      <HomeContainer />
     </div>
   );
 }
