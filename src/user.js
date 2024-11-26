35c9858d334a44be4c1d78a8dfed311ddc6c69f6
vrushabh <vrushabhp702@gmail.com>
2024-11-26 18:22:35 +0530

    created folder structure with props eg

---
+++ src/user.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const User = (props) => {
+  const { data } = props;
+  return (
+    <div>
+      <h1>User Command</h1>
+      <h4>{data.name}</h4>
+      <h4>{data.age}</h4>
+    </div>
+  );
+};
+
+export default User;
